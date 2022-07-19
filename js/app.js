825a2e99f570ad0189a4dd19a264a1b78c67bfa7
FlaviaEvelyn44 <evelynflavia44@gmail.com>
2022-07-19 09:41:41 -0300

    atualizacoes de validacao de  campos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 function pessoa(tipo){
     if(tipo=="fisica"){
-        document.getElementById("fisica").show;
-        document.getElementsByClassName("juridica").hide;
+        $(".juridica").hide();
     }else if(tipo=="juridica"){
-        document.getElementById("fisica").show;
-        document.getElementsByClassName("juridica").hide;
+        $(".juridica").show();
     }
 }
 
@@ -77,43 +75,6 @@ $(document).ready(function () {
 	});
 });
 
-/*function validarCPF(cpf) {	
-	cpf = cpf.replace(/[^\d]+/g,'');	
-	if(cpf == '') return false;	
-		
-	if (cpf.length != 11 || 
-		cpf == "00000000000" || 
-		cpf == "11111111111" || 
-		cpf == "22222222222" || 
-		cpf == "33333333333" || 
-		cpf == "44444444444" || 
-		cpf == "55555555555" || 
-		cpf == "66666666666" || 
-		cpf == "77777777777" || 
-		cpf == "88888888888" || 
-		cpf == "99999999999")
-			return false;		
-		
-	add = 0;	
-	for (i=0; i < 9; i ++)		
-		add += parseInt(cpf.charAt(i)) * (10 - i);	
-		rev = 11 - (add % 11);	
-		if (rev == 10 || rev == 11)		
-			rev = 0;	
-		if (rev != parseInt(cpf.charAt(9)))		
-			return false;		
-		
-	add = 0;	
-	for (i = 0; i < 10; i ++)		
-		add += parseInt(cpf.charAt(i)) * (11 - i);	
-	rev = 11 - (add % 11);	
-	if (rev == 10 || rev == 11)	
-		rev = 0;	
-	if (rev != parseInt(cpf.charAt(10)))
-		return false;		
-	return true;   
-}
-*/
 
 //validaçao usando jquery validate
 $(document).ready(function(){
@@ -128,8 +89,76 @@ $(document).ready(function(){
 			},
 			cnpj:{
 				required:true,
+				cnpjBR:true,
+			},
+		    email:{
+				required:true,
+				email:true,
 			}
 		}
 	})
 })
 
+const btenviar = document.getElementById("btenviar")
+
+form.addEventListener("submit", (event) => {
+
+	const cpf = document.getElementById('cpf')
+
+	if (cpf.value == false) {
+        cpf.classList.add("is-invalid");
+        cpf.focus()
+    } else {
+        cpf.classList.remove("is-invalid")
+    }
+})
+
+
+/**jQuery.validator.setDefaults({
+	debug: true,
+	success: "valid"
+});
+
+
+$( ".validarSenha" ).validate({
+	rules: {
+	password: "required",
+	password_again: {
+		equalTo: "#password"
+	}
+	}
+});
+
+
+
+function validarSenha() {
+    $('#senha').value();
+	$('#confirmarSenha').value();
+
+	if (senha != confirmarSenha) {
+	    confirmarSenha.setCustomValidity("Senhas diferentes!");
+	    return false;
+	} else {
+	    return true;
+	}
+}
+*/
+
+function verificaForcaSenha() 
+{
+	var numeros = /([0-9])/;
+	var alfabeto = /([a-zA-Z])/;
+	var chEspeciais = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
+
+	if($('.validarSenha').val().length<6) 
+	{
+		$('#password-status').html("<span style='color:red'>Fraco, insira no mínimo 6 caracteres</span>");
+	} else {  	
+		if($('.validarSenha').val().match(numeros) && $('.validarSenha').val().match(alfabeto) && $('.validarSenha').val().match(chEspeciais))
+		{            
+			$('#password-status').html("<span style='color:green'><b>Forte</b></span>");
+		} else {
+			$('#password-status').html("<span style='color:orange'>Médio, insira um caracter especial</span>");
+		}
+	}
+}
