6eb6b0076c7c9ad4e189dbf63c2e5474edd65157
David Jefferson <davidjeffersonbx@gmail.com>
2022-07-21 12:12:40 -0300

    ajuste formulario

--- js/app.js
+++ js/app.js
@@ -1,92 +1,103 @@
-//esconder pessoa juridica
-function pessoa(tipo) {
-	if (tipo == "fisica") {
-		$(".juridica").hide();
-	} else if (tipo == "juridica") {
-		$(".juridica").show();
-	}
-};
+var formulario = {
+	iniciar: function () {
+		this.mascaras();
+		this.events();
+		this.formValidate();
+	},
 
-//mascaras
-$(document).ready(function () {
-	$("#tele").mask('(00)00000-0000', {
-		placeholder: '(__)_____-____'
-	});
-	$("#cep").mask('00.000-000', {
-		placeholder: '__.___-___'
-	});
-	$("#cpf").mask('000.000.000-00', {
-		placeholder: '___.___.___-__'
-	});
-	$("#cnpj").mask('00.000.000/0000-00', {
-		placeholder: '___.___.___/____-__'
-	});
-});
+	events: function () {
+		$("#cep").on('blur', function () {
+			formulario.buscarCep($(this));
+		});
 
-// validaçao do cep
-function limpaCep() {
-	// Limpa valores do formulário de cep.
-	$("#logradouro").val("");
-	$("#cidade").val("");
-	$("#bairro").val("");
-}
+		$('#form').on("submit", (event) => {
+			event.preventDefault();
+			formulario.salvar();
+		});
+	},
 
+	mascaras: function () {
+		$("#tele").mask('(00)00000-0000', {
+			placeholder: '(__)_____-____'
+		});
+		$("#cep").mask('00.000-000', {
+			placeholder: '__.___-___'
+		});
+		$("#cpf").mask('000.000.000-00', {
+			placeholder: '___.___.___-__'
+		});
+		$("#cnpj").mask('00.000.000/0000-00', {
+			placeholder: '___.___.___/____-__'
+		});
+	},
 
-//validaçao usando jquery validate
-$(document).ready(function () {
-	//Quando o campo cep perde o foco.
-	$("#cep").blur(function () {
+	formValidate: function () {
+		$("#form").validate({
+			rules: {
+				cpf: {
+					required: true,
+					cpfBR: true,
+					maxlength: 14,
+					minlength: 14,
+				},
+				cnpj: {
+					required: true,
+					cnpjBR: true,
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				senha: {
+					required: true,
+					strongPassword: formulario.strongPassword
+				},
+				senha2: {
+					required: true,
+					equalTo: "#senha",
+				}
+			}
+		});
+	},
 
-		//Nova variável "cep" somente com dígitos.
-		var cep = $(this).val().replace(/\D/g, '');
+	pessoa: function (tipo) {
+		if (tipo == "fisica") {
+			$(".juridica").hide();
+		} else if (tipo == "juridica") {
+			$(".juridica").show();
+		}
+	},
 
-		//Verifica se campo cep possui valor informado.
+	buscarCep: function (element) {
+		var cep = element.val().replace(/\D/g, '');
 		if (cep != "") {
-
-			//Expressão regular para validar o CEP.
 			var validacep = /^[0-9]{8}$/;
-
-			//Valida o formato do CEP.
 			if (validacep.test(cep)) {
-
-				//Preenche os campos com "..." enquanto consulta webservice.
 				$("#logradouro").val("...");
 				$("#cidade").val("...");
 				$("#bairro").val("...");
 				$("#estado").val("...");
-
-				//Consulta o webservice viacep.com.br/
 				$.getJSON("https://viacep.com.br/ws/" + cep + "/json/", function (dados) {
-
 					if (!("erro" in dados)) {
-						//Atualiza os campos com os valores da consulta. 
 						$("#logradouro").val(dados.logradouro);
 						$("#cidade").val(dados.localidade);
 						$("#bairro").val(dados.bairro);
 						$("#estado").val(dados.uf);
-					} //end if.
-					else {
-						//CEP pesquisado não foi encontrado.
-						limpaCep();
+					} else {
+						formulario.limpaCep();
 						alert("CEP não encontrado.");
 					}
 				});
-			} //end if.
-			else {
-				//cep é inválido.
-				limpaCep();
+			} else {
+				formulario.limpaCep();
 				alert("Formato de CEP inválido.");
 			}
-		} //end if.
-		else {
-			//cep sem valor, limpa formulário.
-			limpaCep();
+		} else {
+			formulario.limpaCep();
 		}
-	});
+	},
 
-	$('#form').on("submit", (event) => {
-		event.preventDefault();
-
+	salvar: function () {
 		const cpf = $('#cpf');
 		const email = $('#email');
 		const nome = $('#nome');
@@ -102,147 +113,129 @@ $(document).ready(function () {
 		const estado = $('#estado');
 		const tele = $('#tele');
 
-
-		if (cpf.value == false) {
+		if (!cpf.val().length) {
 			cpf.addClass("is-invalid");
 			cpf.focus();
 		} else {
 			cpf.removeClass("is-invalid");
 		}
 
-		if (email.value == false) {
+		if (!email.val().length) {
 			email.addClass("is-invalid")
 			email.focus()
 		} else {
 			email.removeClass("is-invalid");
 		}
 
-		if (nome.value == false) {
+		if (!nome.val().length) {
 			nome.addClass("is-invalid");
 			nome.focus();
 		} else {
 			nome.removeClass("is-invalid");
 		}
 
-		if (sobrenome.value == false) {
+		if (!sobrenome.val().length) {
 			sobrenome.addClass("is-invalid");
 			sobrenome.focus();
 		} else {
 			sobrenome.removeClass("is-invalid");
 		}
 
-		if (razaoSocial.value == false) {
+		if (!razaoSocial.val().length) {
 			razaoSocial.addClass("is-invalid");
 			razaoSocial.focus();
 		} else {
 			razaoSocial.removeClass("is-invalid");
 		}
 
-		if (nomeFantasia.value == false) {
+		if (!nomeFantasia.val().length) {
 			nomeFantasia.addClass("is-invalid");
 			nomeFantasia.focus();
 		} else {
 			nomeFantasia.removeClass("is-invalid");
 		}
 
-		if (cnpj.value == false) {
+		if (!cnpj.val().length) {
 			cnpj.addClass("is-invalid");
 			cnpj.focus();
 		} else {
 			cnpj.removeClass("is-invalid");
 		}
 
-		if (logradouro.value == false) {
+		if (!logradouro.val().length) {
 			logradouro.addClass("is-invalid");
 			logradouro.focus();
 		} else {
 			logradouro.removeClass("is-invalid");
 		}
 
-		if (cidade.value == false) {
+		if (!cidade.val().length) {
 			cidade.addClass("is-invalid");
 			cidade.focus();
 		} else {
 			cidade.removeClass("is-invalid");
 		}
 
-		if (bairro.value == false) {
+		if (!bairro.val().length) {
 			bairro.addClass("is-invalid");
 			bairro.focus();
 		} else {
 			bairro.removeClass("is-invalid");
 		}
 
-		if (numero.value == false) {
+		if (!numero.val().length) {
 			numero.addClass("is-invalid");
 			numero.focus();
 		} else {
 			numero.removeClass("is-invalid");
 		}
 
-		if (cep.value == false) {
+		if (!cep.val().length) {
 			cep.addClass("is-invalid");
 			cep.focus();
 		} else {
 			cep.removeClass("is-invalid");
 		}
 
-		if (estado.value == "Escolher...") {
+		if (!estado.val().length == "Escolher...") {
 			estado.addClass("is-invalid");
 			estado.focus();
 		} else {
 			estado.removeClass("is-invalid");
 		}
 
-		if (tele.value == false) {
+		if (!tele.val().length) {
 			tele.addClass("is-invalid");
 			tele.focus();
 		} else {
 			tele.removeClass("is-invalid");
 		}
-	});
+	},
 
-	$("#form").validate({
-		rules: {
-			// cpf:{
-			// 	required:true,
-			// 	cpfBR:true,
-			// 	maxlength:14,
-			// 	minlength:14,
-			// },
-			// cnpj:{
-			// 	required:true,
-			// 	cnpjBR:true,
-			// },
-			// email:{
-			// 	required:true,
-			// 	email:true
-			// },
-			senha: {
-				required: true,
-				strongPassword: true,
-			},
-			senha2: {
-				required: true,
-				equalTo: "#senha",
-			}
-		}
-	});
+	limpaCep: function () {
+		// Limpa valores do formulário de cep.
+		$("#logradouro").val("");
+		$("#cidade").val("");
+		$("#bairro").val("");
+	},
 
-	//força da senha
-	function verificaForcaSenha() {
+	strongPassword: function () {
 		var numeros = /([0-9])/;
 		var alfabeto = /([a-zA-Z])/;
 		var chEspeciais = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
 
-		if ($('.validarSenha').val().length < 6) {
+		if ($('#senha').val().length < 6) {
 			$('#password-status').html("<span style='color:red'>Fraco, insira no mínimo 6 caracteres</span>");
 		} else {
-			if ($('.validarSenha').val().match(numeros) && $('.validarSenha').val().match(alfabeto) && $('.validarSenha').val().match(chEspeciais)) {
+			if ($('#senha').val().match(numeros) && $('#senha').val().match(alfabeto) && $('#senha').val().match(chEspeciais)) {
 				$('#password-status').html("<span style='color:green'><b>Forte</b></span>");
 			} else {
 				$('#password-status').html("<span style='color:orange'>Médio, insira um caracter especial</span>");
 			}
 		}
 	}
+};
+
+$(document).ready(function () {
+	formulario.iniciar();
 });
