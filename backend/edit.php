b9d633bbbb1a0b654e1d57144cda469b1af6cbb5
FlaviaEvelyn44 <evelynflavia44@gmail.com>
2022-08-12 12:39:33 -0300

    alteracoes formulario

--- backend/edit.php
+++ backend/edit.php
@@ -1,38 +1,231 @@
 <?php
 
-include("conexao.php");
+    include("conexao.php");
 
-if(!empty($_GET['id_pessoa'])){
+    if(!empty($_GET['id_pessoa'])){
 
-    $id = $_GET['id_pessoa'];
+        $id = $_GET['id_pessoa'];
 
-    $sqlSelect = "SELECT * FROM pessoa WHERE id_pessoa=$id";
+        $sqlSelect = "SELECT * FROM pessoa WHERE id_pessoa=$id";
 
-    $result = $conexao->query($sqlSelect);
+        $result = $conexao->query($sqlSelect);
 
-    if($result->num_rows > 0) {
+        if($result->num_rows > 0) {
 
-        while($row_usuario = mysqli_fetch_assoc($result)){
+            while($row_usuario = mysqli_fetch_assoc($result)){
 
-            $nome = $row_usuario["no_pessoa"];
-            $sobrenome = $row_usuario["ds_sobrenome"];
-            $email = $row_usuario["ds_email"];
-            $tipo = $row_usuario["co_tipo_pessoa"];
-            $cpf = $row_usuario["ds_cpf"];
-            $cnpj = $row_usuario["ds_cnpj"];
-            $cep = $row_usuario["ds_cep"];
-            $logradouro = $row_usuario["ds_logradouro"];
-            $bairro = $row_usuario["ds_bairro"];
-            $cidade =$row_usuario["ds_cidade"];
-            $estado = $row_usuario["co_uf"];
-            $telefone = $row_usuario["ds_telefone"];
-            $numero = $row_usuario["ds_numero"];
-            $sexo = $row_usuario["id_sexo"];
-            $data_nasc = $row_usuario ["dt_nascimento"];
+                $nome = $row_usuario["no_pessoa"];
+                $sobrenome = $row_usuario["ds_sobrenome"];
+                $email = $row_usuario["ds_email"];
+                $tipo = $row_usuario["co_tipo_pessoa"];
+                $cpf = $row_usuario["ds_cpf"];
+                $cnpj = $row_usuario["ds_cnpj"];
+                $cep = $row_usuario["ds_cep"];
+                $logradouro = $row_usuario["ds_logradouro"];
+                $bairro = $row_usuario["ds_bairro"];
+                $cidade =$row_usuario["ds_cidade"];
+                $estado = $row_usuario["co_uf"];
+                $telefone = $row_usuario["ds_telefone"];
+                $numero = $row_usuario["ds_numero"];
+                $sexo = $row_usuario["co_sexo"];
+                $data_nasc = $row_usuario ["dt_nascimento"];
+                }
+        }
+        else
+        {
+            header('Location: /formulario/formulario.php');
+        }
 
-}    
-}
-}
-
+    }
 
 ?>
+
+<a href="http://localhost/formulario/backend/tabela.php">Voltar para usuários</a>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar usuário</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
+    
+
+</head>
+
+<body>
+    <div class="container pt-7">
+        <div class="row d-flex justify-content-center">
+            <div class="col-lg-6">
+                <h1 class="text-center">Editar usuário</h1>
+                <form id="form" class="form" action="backend\cadastro.php" method="POST">
+                    <div class="row mb-3">
+                        <div class="form-group">
+                            <div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" id="radioFisica" name="pessoaTipo"
+                                        value="fisica" onclick="formulario.pessoa(this.value);">
+                                    <label class="form-check-label" for="radioFisica">Pessoa Fisica</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" id="radioJuridica" name="pessoaTipo"
+                                        value="juridica" onclick="formulario.pessoa(this.value);">
+                                    <label class="form-check-label" for="radioJuridica">Pessoa Juridica</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label class="control-label col-sm-2" for="nome">Nome</label>
+                            <input type="text" class="form-control" id="nome" name="nome" value=<?php echo $nome;?> >
+                        </div>
+                        <div class="col-md-6">
+                            <label class="control-label col-sm-2" for="sobrenome">Sobrenome</label>
+                            <input type="text" class="form-control" id="sobrenome" name="sobrenome" value=<?php echo $sobrenome;?> required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label class="form-control-label col-sm-2" for="CPF">CPF</label>
+                            <input type="text" class="form-control cpf" id="cpf" name="cpf" value=<?php echo $cpf;?>>
+                        </div>
+                        <div class="col-md-6 juridica">
+                            <label class="form-control-label col-sm-2" for="cnpj">CNPJ</label>
+                            <input type="text" class="form-control cnpj" id="cnpj" name="cnpj" value=<?php echo $cnpj;?>>
+                        </div>
+                    </div>
+                    <div class="row mb-3 juridica">
+                        <div class="col-md-6 ">
+                            <label class="control-label col-sm-6" for="razaoSocial">Razão Social</label>
+                            <input type="text" class="form-control" id="razaoSocial" name="razaoSocial" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="control-label" for="nomefantasia">Nome Fantasia</label>
+                            <input type="text" class="form-control col-lg-6" id="nomeFantasia" name="nomefantasia"
+                                required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label class="control-label" for="dataNascimento">Data de Nascimento</label>
+                            <input type="date" class="form-control col-lg-6" id="dataNascimento" name="dataNascimento" value=<?php echo $data_nasc;?>>
+                        </div>
+                        <div class="form-group md-6 required">
+                            <label for="sexo">Sexo</label >
+                            <div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="sexo" id="masculino" value="M">
+                                    <label class="form-check-label" for="masculino">Masculino</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="sexo" id="feminino" value="F">
+                                    <label class="form-check-label" for="feminino">Feminino</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="sexo" id="outro" value="O">
+                                    <label class="form-check-label" for="outro">Prefiro não informar</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="tele">Telefone</label>
+                            <input type="tel" id="tele" name="tele" class="form-control" value=<?php echo $telefone;?> required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="email">Email</label>
+                            <input class="form-control" type="email" name="email" id="email"
+                                placeholder="Digite seu email..." value=<?php echo $email;?>>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="cep">CEP</label>
+                            <input class="form-control" type="text" name="cep" id="cep" value=<?php echo $cep;?> required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="estado">Estado</label>
+                            <select class="form-select form-select-sm-4" name="estado" id="estado">
+                                <option selected>Escolher...</option>
+                                <option value="AC">AC</option>
+                                <option value="AL">AL</option>
+                                <option value="AP">AP</option>
+                                <option value="AM">AM</option>
+                                <option value="BA">BA</option>
+                                <option value="CE">CE</option>
+                                <option value="DF">DF</option>
+                                <option value="ES">ES</option>
+                                <option value="GO">GO</option>
+                                <option value="MA">MA</option>
+                                <option value="MT">MT</option>
+                                <option value="MS">MS</option>
+                                <option value="MG">MG</option>
+                                <option value="PA">PA</option>
+                                <option value="PB">PB</option>
+                                <option value="PR">PR</option>
+                                <option value="PE">PE</option>
+                                <option value="PI">PI</option>
+                                <option value="RJ">RJ</option>
+                                <option value="RN">RN</option>
+                                <option value="RS">RS</option>
+                                <option value="RO">RO</option>
+                                <option value="RR">RR</option>
+                                <option value="SC">SC</option>
+                                <option value="SP">SP</option>
+                                <option value="SE">SE</option>
+                                <option value="TO">TO</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="logradouro">Logradouro</label>
+                            <input class="form-control" type="text" name="logradouro" id="logradouro"
+                                placeholder="Logradouro" value=<?php echo $logradouro;?>>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="cidade">Cidade</label>
+                            <input class="form-control" type="text" name="cidade" id="cidade" placeholder="Cidade" value=<?php echo $cidade;?>>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="bairro">Bairro</label>
+                            <input class="form-control" type="text" name="bairro" id="bairro" placeholder="Bairro" value=<?php echo $bairro;?>>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="numero">Número</label>
+                            <input class="form-control" type="text" name="numero" id="numero" placeholder="Número"
+                                required  value=<?php echo $numero;?>>
+                        </div>
+                        
+                    <div class="form-group mb-3">
+                        <button type="submit" id="btenviar" name="btenviar" class="btn btn-primary">Salvar alteraões</button>
+                        <button type="reset" class="btn btn-outline-primary">Limpar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/jquery.mask.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="js/additional-methods.js"></script>
+    <script type="text/javascript" src="js/localization/messages_pt_BR.js"></script>
+
+</body>
+
+</html>
---
+++ backend/js/app.js
@@ -0,0 +1,222 @@
+var formulario = {
+	iniciar: function () {
+		this.mascaras();
+		this.events();
+		this.formValidate();
+	},
+	events: function () {
+		$("#cep").on('blur', function () {
+			formulario.buscarCep($(this));
+		} ) ;
+
+		$ ( '#form' ) . on ( "enviar" ,  ( evento )  =>  {
+			evento . preventDefault ( ) ;
+			formulário . salvar ( ) ;
+		} ) ;
+	} ,
+	mascaras: function () {
+		$("#tele").mask('(00)00000-0000', {
+			placeholder: '(__)_____-____'
+		});
+		$("#cep").mask('00.000-000', {
+			placeholder: '__.___-___'
+		});
+		$("#cpf").mask('000.000.000-00', {
+			placeholder: '___.___.___-__'
+		});
+		$("#cnpj").mask('00.000.000/0000-00', {
+			placeholder: '___.___.___/____-__'
+		});
+	
+	},
+	formValidate: function () {
+		$("#form").validate({
+			rules: {
+				cpf: {
+					required: true,
+					cpfBR: true,
+					maxlength: 14,
+					minlength: 14,
+				},
+				cnpj: {
+					required: true,
+					cnpjBR: true,
+				},
+				email: {
+					required: true,
+					email: true,
+				},
+				senha: {
+					required: true,
+					strongPassword: formulario.strongPassword
+				},
+				senha2 : {
+					required : true,
+					equalTo : "#senha",
+				} ,
+				termos : {
+					required : true,
+				}
+			
+			}
+		} ) ;
+	},
+	pessoa: function (tipo) {
+		if (tipo == "fisica") {
+			$(".juridica").hide();
+		} else if (tipo == "juridica") {
+			$(".juridica").show();
+		}
+	},
+	buscarCep: function (element) {
+		var cep = element.val().replace(/\D/g, '');
+		if (cep != "") {
+			var validacep = /^[0-9]{8}$/;
+			if (validacep.test(cep)) {
+				$("#logradouro").val("...");
+				$("#cidade").val("...");
+				$("#bairro").val("...");
+				$("#estado").val("...");
+				$.getJSON("https://viacep.com.br/ws/" + cep + "/json/", function (dados) {
+					if (!("erro" in dados)) {
+						$("#logradouro").val(dados.logradouro);
+						$("#cidade").val(dados.localidade);
+						$("#bairro").val(dados.bairro);
+						$("#estado").val(dados.uf);
+					} else {
+						formulario.limpaCep();
+						alert("CEP não encontrado.");
+					}
+				});
+			} else {
+				formulario.limpaCep();
+				alert("Formato de CEP inválido.");
+			}
+		} else {
+			formulario.limpaCep();
+		}
+	},
+	salvar: function () {
+		const cpf = $('#cpf');
+		const email = $('#email');
+		const nome = $('#nome');
+		const sobrenome = $('#sobrenome');
+		const razaoSocial = $('#razaoSocial');
+		const nomeFantasia = $('#nomeFantasia');
+		const cnpj = $('#cnpj');
+		const logradouro = $('#logradouro');
+		const cidade = $('#cidade');
+		const bairro = $('#bairro');
+		const numero = $('#numero');
+		const cep = $('#cep');
+		const estado = $('#estado');
+		const tele = $('#tele');
+		if (!cpf.val().length) {
+			cpf.addClass("is-invalid");
+			cpf.focus();
+		} else {
+			cpf.removeClass("is-invalid");
+		}
+		if (!email.val().length) {
+			email.addClass("is-invalid")
+			email.focus()
+		} else {
+			email.removeClass("is-invalid");
+		}
+		if (!nome.val().length) {
+			nome.addClass("is-invalid");
+			nome.focus();
+		} else {
+			nome.removeClass("is-invalid");
+		}
+		if (!sobrenome.val().length) {
+			sobrenome.addClass("is-invalid");
+			sobrenome.focus();
+		} else {
+			sobrenome.removeClass("is-invalid");
+		}
+		if (!razaoSocial.val().length) {
+			razaoSocial.addClass("is-invalid");
+			razaoSocial.focus();
+		} else {
+			razaoSocial.removeClass("is-invalid");
+		}
+		if (!nomeFantasia.val().length) {
+			nomeFantasia.addClass("is-invalid");
+			nomeFantasia.focus();
+		} else {
+			nomeFantasia.removeClass("is-invalid");
+		}
+		if (!cnpj.val().length) {
+			cnpj.addClass("is-invalid");
+			cnpj.focus();
+		} else {
+			cnpj.removeClass("is-invalid");
+		}
+		if (!logradouro.val().length) {
+			logradouro.addClass("is-invalid");
+			logradouro.focus();
+		} else {
+			logradouro.removeClass("is-invalid");
+		}
+		if (!cidade.val().length) {
+			cidade.addClass("is-invalid");
+			cidade.focus();
+		} else {
+			cidade.removeClass("is-invalid");
+		}
+		if (!bairro.val().length) {
+			bairro.addClass("is-invalid");
+			bairro.focus();
+		} else {
+			bairro.removeClass("is-invalid");
+		}
+		if (!numero.val().length) {
+			numero.addClass("is-invalid");
+			numero.focus();
+		} else {
+			numero.removeClass("is-invalid");
+		}
+		if (!cep.val().length) {
+			cep.addClass("is-invalid");
+			cep.focus();
+		} else {
+			cep.removeClass("is-invalid");
+		}
+		if (!estado.val().length == "Escolher...") {
+			estado.addClass("is-invalid");
+			estado.focus();
+		} else {
+			estado.removeClass("is-invalid");
+		}
+		if (!tele.val().length) {
+			tele.addClass("is-invalid");
+			tele.focus();
+		} else {
+			tele.removeClass("is-invalid");
+		}
+	},
+	limpaCep: function () {
+		// Limpa valores do formulário de cep.
+		$("#logradouro").val("");
+		$("#cidade").val("");
+		$("#bairro").val("");
+	},
+	strongPassword: function () {
+		var numeros = /([0-9])/;
+		var alfabeto = /([a-zA-Z])/;
+		var chEspeciais = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
+		if ($('#senha').val().length < 6) {
+			$('#password-status').html("<span style='color:red'>Fraco, insira no mínimo 6 caracteres</span>");
+		} else {
+			if ($('#senha').val().match(numeros) && $('#senha').val().match(alfabeto) && $('#senha').val().match(chEspeciais)) {
+				$('#password-status').html("<span style='color:green'><b>Forte</b></span>");
+			} else {
+				$('#password-status').html("<span style='color:orange'>Médio, insira um caracter especial</span>");
+			}
+		}
+	}
+};
+$(document).ready(function () {
+	formulario.iniciar();
+});
